4294d6cb69d8be039a9edb1f095f9c15ddeb06e4
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-08-24 18:32:07 -0300

    refactort: adding eslint and prettier

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,15 +1,9 @@
 export class User {
-  id?: number;
-  name: string;
-  email: string;
-  password: string;
-  created_at: Date;
-
-  constructor(name: string, email: string, password: string, created_at: Date, id?: number) {
-      this.name = name;
-      this.email = email;
-      this.password = password;
-      this.created_at = created_at;
-      this.id = id;
-  }
+  constructor(
+    public name: string,
+    public email: string,
+    public password: string,
+    public createdAt?: Date,
+    public id?: number
+  ) {}
 }
