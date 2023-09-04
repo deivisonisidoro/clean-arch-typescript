23193278eb87219eed12a1b231d13f19e03bd8f9
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-09-04 10:48:29 -0300

    feat: adding getter and setter

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,46 +1,46 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from 'uuid'
 
 export interface UserInterface {
-  id?: string;
-  name: string;
-  email: string;
-  password: string;
-  createdAt?: Date;
+  id?: string
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
 }
 
-export class User{
-  private readonly _id: string;
-  private _name: string;
-  private _email: string;
-  private _password: string;
-  protected _createdAt: Date;
+export class User {
+  private readonly _id: string
+  private _name: string
+  private _email: string
+  private _password: string
+  protected _createdAt: Date
 
   get id(): string {
-    return this._id;
+    return this._id
   }
 
   get name(): string {
-    return this._name;
+    return this._name
   }
 
   get email(): string {
-    return this._email;
+    return this._email
   }
 
   get password(): string {
-    return this._password;
+    return this._password
   }
 
   get createdAt(): Date {
-    return this._createdAt;
+    return this._createdAt
   }
 
   set name(newName: string) {
-    this._name = newName;
+    this._name = newName
   }
 
   set email(newEmail: string) {
-    this._email = newEmail;
+    this._email = newEmail
   }
 
   set password(newPassword: string) {
@@ -48,14 +48,14 @@ export class User{
   }
 
   set createdAt(newDate: Date) {
-    this._createdAt = newDate;
+    this._createdAt = newDate
   }
 
   constructor(props: UserInterface) {
-    this._name = props.name;
-    this._password = props.password;
-    this._email = props.email;
-    this._createdAt = props.createdAt || new Date();
-    this._id = props.id || uuid();
+    this._name = props.name
+    this._password = props.password
+    this._email = props.email
+    this._createdAt = props.createdAt || new Date()
+    this._id = props.id || uuid()
   }
 }
