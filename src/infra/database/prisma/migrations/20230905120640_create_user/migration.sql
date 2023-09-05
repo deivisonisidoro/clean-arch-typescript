6ede775c93a36ddf90d84fee757f09f216718dc0
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-09-05 09:14:39 -0300

    feat: adding database using prisma

---
+++ src/infra/database/prisma/migrations/20230905120640_create_user/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
