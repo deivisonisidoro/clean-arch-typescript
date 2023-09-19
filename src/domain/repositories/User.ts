5845c28efc6b3c9e5cfb385775ce7e6d04a49ca7
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-09-19 17:53:07 -0300

    fix: fixing the imports

--- src/domain/repositories/User.ts
+++ src/domain/repositories/User.ts
@@ -1,7 +1,7 @@
-import { PaginationDTO } from '../../applications/dtos/Pagination'
-import { ICreateUserRequestDTO } from '../../applications/dtos/User/CreateUser'
-import { IUpdateUserRequestDTO } from '../../applications/dtos/User/UpdateUser'
-import { IUserOutRequestDTO } from '../../applications/dtos/User/UserOut'
+import { PaginationDTO } from '../dtos/Pagination'
+import { ICreateUserRequestDTO } from '../dtos/User/CreateUser'
+import { IUpdateUserRequestDTO } from '../dtos/User/UpdateUser'
+import { IUserOutRequestDTO } from '../dtos/User/UserOut'
 
 export interface IUsersRepository {
   create(data: ICreateUserRequestDTO): Promise<IUserOutRequestDTO>
