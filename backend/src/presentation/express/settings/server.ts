bc2e13c6f4a51cc4df687cf8f8f299f77347f03d
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-11-11 19:07:00 -0300

    refactor: changing the text

--- backend/src/presentation/express/settings/server.ts
+++ backend/src/presentation/express/settings/server.ts
@@ -2,4 +2,4 @@ import { app } from './app'
 
 const PORT = process.env.PORT || 3333
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`))
