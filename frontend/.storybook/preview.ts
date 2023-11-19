d462a5f54590c0ed868f72f97af3ea8274d2054e
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-11-19 09:00:15 -0300

    refactor: removing imports

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,8 +1,6 @@
 import type { Preview } from "@storybook/react";
 import "../src/app/globals.css"
 
-import '../src/app/globals.css';
-
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
