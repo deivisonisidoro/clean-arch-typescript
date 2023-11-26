ec2179bf48e1f4702930868bd22c3a99f101c8f9
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-11-26 07:57:50 -0300

    feat: adding refresh Token User Composer

--- backend/src/infra/providers/TokenManager.ts
+++ backend/src/infra/providers/TokenManager.ts
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export class TokenManagerProvider {
-  static validateTokenAge(expires_in: number): boolean {
+  validateTokenAge(expires_in: number): boolean {
     return dayjs().isAfter(dayjs.unix(expires_in));
   }
 
-  static validateToken(token: string): boolean {
+  validateToken(token: string): boolean {
     try {
       verify(token, process.env.API_SECRET || '');
       return true;
